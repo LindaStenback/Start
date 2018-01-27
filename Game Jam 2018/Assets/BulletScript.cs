f9df034cba2808d9936a8d28ad8fba60f692ba9c
Maison122 <MFLEMI204@Caledonian.ac.uk>
2018-01-27 08:30:53 +0000

    Update3

---
+++ Game Jam 2018/Assets/BulletScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour 
+{
+	public float xfire;
+	public float yfire;
+
+	// Use this for initialization
+	void Start () 
+	{
+		xfire = Input.GetAxis ("Right Joystick X");
+		yfire = Input.GetAxis ("Right Joystick Y");
+
+		if (xfire >= 0.2) 
+		{
+			xfire = 5;
+		}
+		if (xfire <= -0.2)
+		{
+			xfire = -5;
+		}
+
+		if (yfire >= -0.2) 
+		{
+			yfire = 5;
+		}
+		if (yfire <= 0.2)
+		{
+			yfire = -5;
+		}
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (xfire, yfire, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
