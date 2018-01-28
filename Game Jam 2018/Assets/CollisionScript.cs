04287520976046fada846deab23faed617ee1290
AndrewMac26 <macfarlane.andrew98@gmail.com>
2018-01-28 01:45:34 +0000

    Immunity Powerup?

--- Game Jam 2018/Assets/CollisionScript.cs
+++ Game Jam 2018/Assets/CollisionScript.cs
@@ -7,22 +7,29 @@ public class CollisionScript : MonoBehaviour {
     public LifeScript playerLifeScript2;
     bool auraBuffP1 = false;
     bool auraBuffP2 = false;
-    public float auraBuffTimerP1 = 10;
-    public float auraBuffTimerP2 = 10;
+
+    public float auraBuffTimerP1;// = 10;
+    public float auraBuffTimerP2;// = 10;
+
+    public float immuneTimerP1;// = 10;
+    public float immuneTimerP2;// = 10;
+
+    bool p1Immune = false;
+    bool p2Immune = false;
 
     public Follow follow;
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        auraBuffTimerP1 -= Time.deltaTime;
-        auraBuffTimerP2 -= Time.deltaTime;
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //auraBuffTimerP1 -= Time.deltaTime;
+        //auraBuffTimerP2 -= Time.deltaTime;
+        PowerupDecay();
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -33,26 +40,31 @@ public class CollisionScript : MonoBehaviour {
                 {
                     if (auraBuffP2 == true)
                     {
-                        
+
                         follow.Player2sBitch = true;
                         follow.Player1sBitch = false;
                     }
                     else
                     {
-                        playerLifeScript.player2Health -= 1;
+                        if (!p2Immune) {
+                            playerLifeScript.player2Health -= 1;
+                        }
                     }
                 }
                 break;
             case "Player2sBitch":
                 if (auraBuffP1 == true)
                 {
-                    
+
                     follow.Player1sBitch = true;
                     follow.Player2sBitch = false;
                 }
                 else
                 {
-                    playerLifeScript2.player1Health -= 1;
+                    if (!p1Immune)
+                    {
+                        playerLifeScript2.player1Health -= 1;
+                    }
                 }
                 break;
             case "AuraBuff":
@@ -61,17 +73,33 @@ public class CollisionScript : MonoBehaviour {
                     auraBuffP1 = true;
                     auraBuffTimerP1 = 10;
                 }
-                else if(this.tag == "Player2")
+                else if (this.tag == "Player2")
                 {
                     auraBuffP2 = true;
                     auraBuffTimerP2 = 10;
                 }
                 break;
-                
+            case "ImmunityBuff":
+                if (this.tag == "Player1")
+                {
+                    p1Immune = true;
+                    immuneTimerP1 = 10f;
+                }
+                else if (this.tag == "Player2")
+                {
+                    p2Immune = true;
+                    immuneTimerP2 = 10f;
+                }
+                break;
+
         }
 
 
-   
+        if (p1Immune)
+        {
+        }
+
+
         //if (this.tag == "Player2" && collision.gameObject.tag == "Player1sBitch")
         //{
         //    playerLifeScript.player2Health -= 1;
@@ -81,6 +109,50 @@ public class CollisionScript : MonoBehaviour {
         //{
         //    playerLifeScript2.player1Health -= 1;
         //}
-        
+
+    }
+
+    //Needs optimized - this is shite / andrew
+    void PowerupDecay()
+
+
+    {
+        if (p1Immune && immuneTimerP1 > 0)
+        {
+            immuneTimerP1 -= Time.deltaTime;
+        }
+
+        if (p2Immune && immuneTimerP2 > 0)
+        {
+            immuneTimerP2 -= Time.deltaTime;
+        }
+
+        if (auraBuffP1 && auraBuffTimerP1 > 0)
+        {
+            auraBuffTimerP1 -= Time.deltaTime;
+        }
+        if (auraBuffP2 && auraBuffTimerP2 > 0)
+        {
+            auraBuffTimerP2 -= Time.deltaTime;
+        }
+
+        if (immuneTimerP1 == 0)
+        {
+            p1Immune = false;
+        }
+        if (immuneTimerP2 == 0)
+        {
+            p2Immune = false;
+        }
+        if (auraBuffTimerP1 == 0)
+        {
+            auraBuffP1 = false;
+        }
+        if (auraBuffTimerP2 == 0)
+        {
+            auraBuffP2 = false;
+        }
+
+
     }
 }
